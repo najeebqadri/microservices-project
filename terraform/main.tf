a7f45f4c88829e2ab83980f26647be03c273584e
Najeeb Qadri <najeebqadri@gmail.com>
2025-07-08 12:28:07 +1000

    commiting changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 locals {
   ami_type               = "AL2_x86_64"
   azs                    = slice(data.aws_availablity_zones.available.names, 0, 3)
-  cluster_name           = ""
+  cluster_name           = "microservice-project"
   capacity_type          = "SPOT"
   cluster_varsion        = "1.33"
   disk_size              = 30
@@ -14,20 +14,21 @@ locals {
   node_min_size          = 1
   intra_subnets          = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
   private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets         = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   single_nat_gateway     = true
   vpc_cidr               = "10.0.0.0/16"
 }
 
-
+data "aws_availability_zones" "available" {}
 module "vpc" {
   source             = "terraform-aws-modules/vpc/aws"
   version            = "6.0.1"
-  name               = ""
-  azs                = ""
-  cidr               = ""
-  intra_subnets      = ""
-  private_subnets    = ""
-  public_subnets     = ""
-  enable_nat_gateway = ""
-  single_nat_gateway = ""
+  name               = "${local.cluster_name}-vpc"
+  azs                = local.azs
+  cidr               = local.vpc_cidr
+  intra_subnets      = local.intra_subnets
+  private_subnets    = local.private_subnets
+  public_subnets     = local.public_subnets
+  enable_nat_gateway = local.enable_nat_gateway
+  single_nat_gateway = local.enable_nat_gateway
 }
